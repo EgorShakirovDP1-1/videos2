4d1ef69f0eb2ba569fc38445bba5e7a2ff772992
Egor.Shakirov@students.rvt.lv <egorsha2005@gmail.com>
2023-10-20 10:09:58 +0300

    improved quality

---
+++ Components/script.vue
@@ -0,0 +1,8 @@
+<script>
+const films = [
+    { title: "Muhtar ebet tatar", director: "Andrey Panin", year: 2020, link: 'images/sexyImage.png' },
+    { title: "School 13", director: "Night Wayfarer", year: 2019, link: 'images/beesechurger' },
+    { title: "zreluya sisastaya baba", director: "Jonny Sus", year: 2018, link: 'images/csquad' },
+];
+
+</script>
